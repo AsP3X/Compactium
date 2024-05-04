298bb0e57f39cd91a5e38c1ea48cfa9833d98dfb
Niklas Vorberg <services@vorberg-webmail.de>
2024-05-04 19:06:47 +0200

    INIT: added all project files

---
+++ src/main/java/de/corespace/compactium/Compactium.java
@@ -0,0 +1,19 @@
+package de.corespace.compactium;
+
+import de.corespace.compactium.item.ModItemGroups;
+import de.corespace.compactium.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Compactium implements ModInitializer {
+	public static final String MOD_ID = "compactium";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/de/corespace/compactium/CompactiumClient.java
@@ -0,0 +1,10 @@
+package de.corespace.compactium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CompactiumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
